98c0afa60581a0dfd547a00e735363f904caad89
r3dd0g1978 <130005368+r3dd0g1978@users.noreply.github.com>
2023-08-02 20:31:19 +1000

    State Machine Updates

--- Udemy RPG Course FullGame/Assets/Scripts/PlayerJumpState.cs
+++ Udemy RPG Course FullGame/Assets/Scripts/PlayerJumpState.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public class PlayerJumpState : PlayerState
 {
     public PlayerJumpState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
@@ -7,6 +9,7 @@ public class PlayerJumpState : PlayerState
     public override void Enter()
     {
         base.Enter();
+        rb.velocity = new Vector2(rb.velocity.x, player.jumpForce);
     }
 
     public override void Exit()
@@ -17,5 +20,8 @@ public class PlayerJumpState : PlayerState
     public override void Update()
     {
         base.Update();
+
+        if (rb.velocity.y < 0)
+            stateMachine.ChangeState(player.airState);
     }
 }
