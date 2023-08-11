7f19303feb18be04a3b9d3ce84108c2a2d8bae18
r3dd0g1978 <130005368+r3dd0g1978@users.noreply.github.com>
2023-08-11 19:02:56 +1000

    Added Flip and FlipController

--- Udemy RPG Course FullGame/Assets/Scripts/Player.cs
+++ Udemy RPG Course FullGame/Assets/Scripts/Player.cs
@@ -13,6 +13,9 @@ public class Player : MonoBehaviour
     [SerializeField] float wallCheckDistance;
     [SerializeField] LayerMask whatIsGround;
 
+    public float facingDir { get; private set; } = 1;
+    private bool facingRight = true;
+
     #region Components
     public Animator anim { get; private set; }
     public Rigidbody2D rb { get; private set; }
@@ -51,6 +54,7 @@ public class Player : MonoBehaviour
     public void SetVelocity(float _xVelocity, float _yVelocity)
     {
         rb.velocity = new Vector2(_xVelocity, _yVelocity);
+        FlipController(_xVelocity);
     }
 
     public bool isGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
@@ -59,5 +63,20 @@ public class Player : MonoBehaviour
     {
         Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y + groundCheckDistance));
         Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+    }
+
+    public void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    public void FlipController(float _x)
+    {
+        if (_x > 0 && !facingRight)
+            Flip();
+        else if (_x < 0 && facingRight)
+            Flip();
     }
 }
